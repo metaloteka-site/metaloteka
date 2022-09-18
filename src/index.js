1e64ade80d48a6ad4e24e03066987c761f564570
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2022-09-18 23:07:18 +0300

    install babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("Как интересно!")
+
+
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
-const path = require('path');
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: '[name].js',
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/template.html'), // шаблон
-            filename: 'index.html', // название выходного файла
-        }),
-    ],
+	entry: {
+		main: path.resolve(__dirname, './src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: '[name].js',
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'webpack Boilerplate',
+			template: path.resolve(__dirname, './src/template.html'), // шаблон
+			filename: 'index.html', // название выходного файла
+		}),
+	],
 }
