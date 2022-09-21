4eee47c645450fe63b5b5e24db690b860e92c7af
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2022-09-22 00:14:29 +0300

    настроил list

---
+++ src/app/list/namePages.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+	names: (page) => {
+		let name;
+		switch (path.basename(page, '.pug')) {
+			case 'index':
+				name = 'Главная'
+				break;
+			default:
+				name = 'name page'
+				break;
+		}
+		return name;
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,68 @@
 const paths = require('./webpack-config/paths')
+const namePages = require('./src/app/list/namePages').names
+const fs = require('fs')
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin');
-const magicImporter = require('node-sass-magic-importer');
+const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin')
+const magicImporter = require('node-sass-magic-importer')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
+let plugins = [];
+let links = [];
+
+fs.readdirSync('./src/pages/').forEach(dirPage => {
+
+	const arrPage = fs.readdirSync(`./src/pages/${dirPage}/`)
+
+	arrPage.forEach(page=> {
+		if (path.extname(page) === ".pug") {
+
+			// Преобразование страниц pug в html
+			plugins.push(
+				new HtmlWebpackPlugin({
+					template: `./src/pages/${dirPage}/${page}`,
+					filename: `./${path.basename(page, '.pug')}.html`,
+					inject: 'body',
+				})
+			)
+
+			// Создание объекта с данными страниц
+			links.push({
+				link: `./${dirPage}.html`,
+				title: dirPage,
+				name: `${namePages(page)}`
+			})
+		}
+	})
+
+})
+
+plugins.push(
+	new HtmlWebpackPlugin({
+		template: `./src/app/list/list.pug`,
+		filename: `./list.html`,
+		inject: 'body',
+	}),
+	new BeautifyHtmlWebpackPlugin({
+		'indent_size': 2,
+		'indent_char': ' ',
+		'indent_with_tabs': true,
+		'editorconfig': true,
+	}),
+	new MiniCssExtractPlugin({
+		filename: 'css/[name].css'
+	}),
+)
+
 module.exports = {
 	entry: {
-		app: `${paths.src}/app.js`
-},
+		app: `${paths.src}/app/app.js`,
+	},
 	output: {
 		path: `${paths.build}`,
-		filename: '[name].js',
+		filename: 'js/[name].js',
+		publicPath: '/',
 		clean: true,
 	},
 	stats: {
@@ -39,16 +90,16 @@ module.exports = {
 				test: /\.pug$/,
 				use: [
 					{
-						loader: "html-loader"
+						loader: 'html-loader'
 					},
 					{
-						loader: "pug-html-loader",
+						loader: 'pug-html-loader',
 						options: {
 							data: {
-								linkslist: 'links'
+								linksList: links
 							},
 							interpolate: true,
-							// pretty: true
+							pretty: false
 						}
 					}
 				]
@@ -96,26 +147,5 @@ module.exports = {
 
 		]
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'webpack Boilerplate',
-			template: `${paths.src}/index.pug`,
-			filename: 'index.html',
-			inject: 'body',
-		}),
-		new HtmlWebpackPlugin({
-			template: `${paths.src}/list.pug`,
-			filename: 'list.html',
-			inject: 'body',
-		}),
-		new BeautifyHtmlWebpackPlugin({
-			"indent_size": 2,
-			"indent_char": " ",
-			"indent_with_tabs": true,
-			"editorconfig": true,
-		}),
-		new MiniCssExtractPlugin({
-			filename: "css/[name].css"
-		}),
-	],
+	plugins: plugins,
 }
