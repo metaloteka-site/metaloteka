d51ce44365e42b7369db584a7f937f7e317bf1d9
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2022-09-29 23:20:09 +0300

    feat: реализация подключение картинок

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin');
 const magicImporter = require('node-sass-magic-importer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 let plugins = [];
 let links = [];
@@ -113,11 +114,14 @@ module.exports = {
 				use: ['babel-loader'],
 			},
 			{
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				test: /\.(jpe?g|png|gif|svg)$/i,
 				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext]',
+				},
 			},
 			{
-				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				test: /\.(woff(2)?|eot|ttf|otf)$/,
 				type: 'asset/resource',
 				generator: {
 					filename: 'fonts/[name][ext]',
@@ -150,6 +154,42 @@ module.exports = {
 					},
 				],
 			},
+		],
+	},
+	optimization: {
+		minimizer: [
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [
+							['gifsicle', { interlaced: true }],
+							['mozjpeg', { quality: 85 }],
+							['pngquant', { optimizationLevel: 6 }],
+							[
+								'svgo',
+								{
+									plugins: [
+										{
+											name: 'preset-default',
+											params: {
+												overrides: {
+													removeViewBox: false,
+													addAttributesToSVGElement: {
+														params: {
+															attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+														},
+													},
+												},
+											},
+										},
+									],
+								},
+							],
+						],
+					},
+				},
+			}),
 		],
 	},
 	plugins: plugins,
