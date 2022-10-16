a77e2ea0231c880b034dfcdebc91edb08c7d7662
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2022-10-16 22:50:49 +0300

    refactor(Tabs): рефакторинг Tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,19 +1,18 @@
 class TabsControl {
 	constructor(elem) {
-		this.tt = elem;
+		this.component = elem;
+		this.tabBtns = this.component.querySelectorAll('.js-tab');
+		this.tabContents = document.querySelectorAll('.tabs__content');
 	}
 
 	tabs() {
-		const tabs = document.querySelectorAll('.js-tab');
-		const tabContents = document.querySelectorAll('.js-tab-content');
-
-		tabs.forEach((tab, index) => {
+		this.tabBtns.forEach((tab, index) => {
 			tab.addEventListener('click', (event) => {
-				tabContents.forEach((tabContent) => tabContent.classList.remove('active'));
-				tabs.forEach((tab) => tab.classList.remove('active'));
+				this.tabContents.forEach((tabContent) => tabContent.classList.remove('active'));
+				this.tabBtns.forEach((tab) => tab.classList.remove('active'));
 
 				event.currentTarget.classList.add('active');
-				tabContents[index].classList.add('active');
+				this.tabContents[index].classList.add('active');
 			});
 		});
 	}
