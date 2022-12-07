4e230f86ba94b060a74e47c9c08ab8d2c3f0da0c
Your Name <you@example.com>
2022-12-07 12:06:53 +0300

    feat(add-tabs): tabs

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,29 +1,29 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper, { Navigation, Pagination } from 'swiper';
 
-class SliderControl {
-	constructor(elem) {
-		this.component = elem;
-	}
+// class SliderControl {
+// 	constructor(elem) {
+// 		this.component = elem;
+// 	}
 
-	// Инициализация класса Slider
-	init() {
-		new Swiper(this.component, {
-			pagination: {
-				el: '.slider__pagination',
-			},
+// 	// Инициализация класса Slider
+// 	init() {
+// 		new Swiper(this.component, {
+// 			pagination: {
+// 				el: '.slider__pagination',
+// 			},
 
-			navigation: {
-				nextEl: '.slider__button-next',
-			},
-			modules: [Navigation, Pagination],
-		});
-	}
-}
+// 			navigation: {
+// 				nextEl: '.slider__button-next',
+// 			},
+// 			modules: [Navigation, Pagination],
+// 		});
+// 	}
+// }
 
-export function Slider() {
-	const elem = document.querySelector('.slider');
+// export function Slider() {
+// 	const elem = document.querySelector('.slider');
 
-	if (elem) {
-		new SliderControl(elem).init();
-	}
-}
+// 	if (elem) {
+// 		new SliderControl(elem).init();
+// 	}
+// }
