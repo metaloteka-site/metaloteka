b130dcb490bb7c7e31a8f7439a087d8d9cd08a5e
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2022-12-20 01:05:07 +0300

    feat: add logic change section block

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,9 @@ import './app.scss';
 // подключение js компонентов
 import { Tabs, Slider, Popup, Accordion, Pagination } from '../components';
 
+// подключение js секций
+import { SectionCatalog } from '../sections/section-catalog/section-catalog';
+
 // Вызов js после загрузки DOM дерева
 window.addEventListener('DOMContentLoaded', () => {
 	Tabs();
@@ -10,4 +13,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	Slider();
 	Popup();
 	Pagination();
+	SectionCatalog();
 });
