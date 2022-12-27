57c2b64269d3a2766d204c445a9cf47664539109
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2022-12-27 11:29:18 +0300

    feat: add ProductSlider

---
+++ src/components/ProductSlider/ProductSlider.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper';
+
+class ProductSliderControl {
+	constructor(elem) {
+		this.component = elem;
+
+		this.init();
+	}
+
+	// Инициализация класса Slider
+	init() {
+		const galleryTop = new Swiper('.gallery-top', {
+			spaceBetween: 10,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			loop: true,
+			loopedSlides: 3,
+		});
+		const galleryThumbs = new Swiper('.gallery-thumbs', {
+			spaceBetween: 10,
+			slidesPerView: 'auto',
+			touchRatio: 0.2,
+			slideToClickedSlide: true,
+			loop: true,
+			loopedSlides: 3,
+		});
+		galleryTop.controller.control = galleryThumbs;
+		galleryThumbs.controller.control = galleryTop;
+	}
+}
+
+export function ProductSlider() {
+	const elem = document.querySelector('.product-swiper');
+
+	if (elem) {
+		new ProductSliderControl(elem);
+	}
+}
