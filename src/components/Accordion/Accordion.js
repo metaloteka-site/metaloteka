82b38c4073f853cd212b5f1a60b9f69de38a3cfa
vladistl <vladis.tl.0880@gmail.com>
2023-01-16 17:11:00 +0300

    поправил аккордеон, чтобы можно было экспортнуть

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,31 +1,4 @@
-class AccordionControl {
-	constructor(block) {
-		this.block = block;
-		this.blockClass = '.accordion';
-
-		this.title = this.block.querySelector(`${this.blockClass}__title`);
-		this.body = this.block.querySelector(`${this.blockClass}__body`);
-
-		this.bodyHeight = ` ${this.body.offsetHeight}px`;
-
-		this.init();
-	}
-
-	init() {
-		this.body.style.height = 0;
-
-		this.title.addEventListener('click', () => {
-			// e.stopPropagation();
-			this.block.classList.toggle('js-active');
-
-			if (this.body.offsetHeight === 0) {
-				this.body.style.height = this.bodyHeight;
-			} else {
-				this.body.style.height = 0;
-			}
-		});
-	}
-}
+import {AccordionControl} from './AccordionControl.js';
 
 export function Accordion() {
 	const elems = document.querySelectorAll('.accordion');
@@ -36,3 +9,4 @@ export function Accordion() {
 		});
 	}
 }
+
